65c650ed355434ee7df91641a8dbb963029b77bd
Rin <burin.ina@student.mahidol.edu>
2022-11-19 12:53:56 +0700

    Initial commit

---
+++ src/main/java/Project3_6480250/MainApplication.java
@@ -0,0 +1,15 @@
+/*
+FirstName, Surname, StudentID
+Burin, Intachuen, 6480250
+Mhadhanagul, Charoenphon, 6381199
+Tanakorn, Mankhetwit, 6480282
+Sittipoj, Techathaicharoen, 6380361
+*/
+package Project3_6480250;
+
+public class MainApplication {
+
+    public static void main(String [] args){
+
+    }
+}
---
+++ src/main/java/Project3_6480250/readme.txt
@@ -0,0 +1,5 @@
+FirstName, Surname, StudentID
+Burin, Intachuen, 6480250
+Mhadhanagul, Charoenphon, 6381199
+Tanakorn, Mankhetwit, 6480282
+Sittipoj, Techathaicharoen, 6380361
