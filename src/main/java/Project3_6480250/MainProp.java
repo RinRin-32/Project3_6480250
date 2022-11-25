a9ca4ab71796ec68c0fce55e89b041b5e1a291a7
burin <burin.ina@student.mahidol.edu>
2022-11-25 15:11:25 +0700

    mainprop constructor

--- src/main/java/Project3_6480250/MainProp.java
+++ src/main/java/Project3_6480250/MainProp.java
@@ -5,37 +5,30 @@ package Project3_6480250;
 import javax.swing.*;
 
 public class MainProp extends JLabel {
-    private ImageIcon faceLeft, faceRight;
+    private ImageIcon goodmanFace;
     private MainApplication parentFrame;
-    private String [] imageFiles ;//haven't added image yet
-    private int width, height;//has to set size here
-    private int curX, curY; //has to set location here
-    private boolean left = true, walk = false, up = true;
+    private String imageFiles = "src/main/java/Project3_6480250/resources/goodman.png";//haven't added image yet
+    private int width = 100, height = 100;//has to set size here
+    private int curX = 640, curY = 360; //has to set location here
+    private boolean walk = false, up = true;
     private int speed;//sleep duration
     //set default values like facing left or right and or movement
 
     public MainProp(MainApplication pf){
         parentFrame = pf;
 
+        goodmanFace = new ImageIcon((imageFiles));
         //set images here
+        setIcon(goodmanFace);
         //set sound
 
         //setIcon(icon);
         setBounds(curX, curY, width, height);
     }
     public void setSpeed(int s) { speed = s;}
-    public void turnLeft() { /*setIcon(icon);*/ left = true;}
-    public void turnRight(){ /*setIcon(icon;*/ left = false;}
     public void setWalk(boolean w) {walk = w;}
     public boolean isWalk() { return walk;}
     public void updateLocation(){
-        if(left){
-            //curX = smth
-            //if on edge frame move to parentFrame.getWidth()
-        }else{
-            //curX = smth
-            //if on edge frame move to curX = 0
-        }
         if(up){
             //curY = smth
             //if on edge frame move to parentFrame.getHeight()
