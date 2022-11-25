9f4b67463f0cfe6f3ecd2e44e77e292b62847b6f
Tanakorn <tanakorn.mak@student.mahidol.edu>
2022-11-26 00:41:41 +0700

    updated Enemy.java, don't really know how to set bound for two different label, pls check

--- src/main/java/Project3_6480250/Enemy.java
+++ src/main/java/Project3_6480250/Enemy.java
@@ -3,21 +3,34 @@ package Project3_6480250;
 import javax.swing.*;
 
 public class Enemy extends JLabel {
-    private ImageIcon faceLeft, faceRight;
+    private ImageIcon jesseEm, waltBo;
     private MainApplication parentFrame;
-    private String [] imageFiles ;//haven't added image yet
-    private int width, height;//has to set size here
-    private int curX, curY; //has to set location here
+    private String [] imageFiles = {"src/main/java/Project3_6480250/resources/jesse.png","src/main/java/Project3_6480250/resources/walt_Boss.png"};//haven't added image yet
+    private String soundFiles = "src/main/java/Project3_6480250/resources/bigpew.mp3";
+
+    private int jwidth = 250, jheight = 359, wWidth = 500, wheight = 418;//has to set size here
+    private int curX, curY = 0; //has to set location here
     private int speed = 500; //set sleep time, editable
+
+    private SoundEffect   shootSound;
 
     public Enemy(MainApplication pf){
         parentFrame = pf;
+
+        curX = (int)(Math.random() * 5555) % (parentFrame.getWidth()-100);
         //set images here
+        jesseEm = new ImageIcon(imageFiles[0]).resize(jwidth, jheight);
+        waltBo = new ImageIcon(imageFiles[1]).resize(wWidth, wheight); //resize()
+
         //set sound
+        shootSound = new SoundEffect(soundFiles);
 
         //setIcon(icon);
-        setBounds(curX, curY, width, height);
+        setIcon(jesseEm);
+        setBounds(curX, curY, jwidth, jheight);
     }
+
+
 
     public void gotHit(){
         //play sound
